6adcc0ff74d6fa90b28e04d1916ea1786e10d000
Faith Muthoni <faith@Faiths-MacBook-Pro.local>
2022-04-05 19:51:36 +0300

    creat models

--- to_do/app_todo/models.py
+++ to_do/app_todo/models.py
@@ -5,3 +5,10 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+class Task(models.Model):
+   
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null = True, blank=True )
+    title =models.CharField(max_length=200,null = True, blank=True)
+    description = models.TextField(null = True, blank=True)
+    complete  = models.BooleanField(default=False)
+    create_at = models.DateTimeField(auto_now_add=True)
