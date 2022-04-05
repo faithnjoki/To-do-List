f1089896efc92b57614a5b85751a7cf6db932609
Faith Muthoni <faith@Faiths-MacBook-Pro.local>
2022-04-05 21:12:21 +0300

    my first class based view

--- to_do/app_todo/views.py
+++ to_do/app_todo/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+from .models import Task
+
+
 
 # Create your views here.
-def tasklist(requests):
-    return HttpResponse('to do list')
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
