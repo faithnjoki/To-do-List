50857c879e05c0ec9cbacfdfb077054906a48d00
Faith Muthoni <faith@Faiths-MacBook-Pro.local>
2022-04-05 21:35:23 +0300

    Add Task Detail view

--- to_do/app_todo/views.py
+++ to_do/app_todo/views.py
@@ -1,5 +1,7 @@
+from asyncio import tasks
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Task
 
 
@@ -8,3 +10,6 @@ from .models import Task
 class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'
+
+class TaskDetail(DetailView):
+    model = Task
