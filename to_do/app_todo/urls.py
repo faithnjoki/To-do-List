138943a16f8576e7d7a1b266108cb4a8cc742983
Faith Muthoni <faith@Faiths-MacBook-Pro.local>
2022-04-06 22:34:27 +0300

    reg url

--- to_do/app_todo/urls.py
+++ to_do/app_todo/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from  .views import TaskList,TaskDetail,TaskCreate,TaskUpdate,DetailView,DeleteView,CustomLoginView
+from  .views import TaskList,TaskDetail,TaskCreate,TaskUpdate,DetailView,DeleteView,CustomLoginView, RegisterPage
 from django.contrib.auth.views import LogoutView
 
 
@@ -7,6 +7,8 @@ urlpatterns = [
     path('login/', CustomLoginView.as_view(), name= 'login'),
     # next_page tell where the page goes after one logs out
     path('logout/', LogoutView.as_view(next_page = 'login'), name= 'logout'),
+    path('register/', RegisterPage.as_view(), name= 'register'),
+
 
     path('', TaskList.as_view(), name = 'tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name = 'task'),
