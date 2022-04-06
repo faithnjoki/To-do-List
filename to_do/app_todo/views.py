f73d4d5a080cff05f959d1d73d58d6bb7de58e2d
Faith Muthoni <faith@Faiths-MacBook-Pro.local>
2022-04-06 16:47:41 +0300

    login view

--- to_do/app_todo/views.py
+++ to_do/app_todo/views.py
@@ -1,15 +1,32 @@
 from asyncio import tasks
 from dataclasses import field
-from django.shortcuts import render
+from pyexpat import model
+from django.shortcuts import redirect, render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView,DeleteView
 from django.urls import reverse_lazy
+# login view 
+from django.contrib.auth.views import LoginView
+
 from .models import Task
 
 
 
+
 # Create your views here.
+class CustomLogiView(LoginView):
+    template_name =  'templates/app_todo/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = 'True'
+
+    # the function overrides the success url am using
+    def get_success_ulrs(self):
+        return reverse_lazy('tasks')
+
+
+
+
 class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'
@@ -28,3 +45,9 @@ class TaskUpdate(UpdateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
+
+class DeleteView(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
+    
