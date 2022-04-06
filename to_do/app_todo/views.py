995297a5fc43da1608f891be71100de495bd861f
Faith Muthoni <faith@Faiths-MacBook-Pro.local>
2022-04-06 10:35:48 +0300

    creatview class

--- to_do/app_todo/views.py
+++ to_do/app_todo/views.py
@@ -1,7 +1,10 @@
 from asyncio import tasks
+from dataclasses import field
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
+from django.urls import reverse_lazy
 from .models import Task
 
 
@@ -13,3 +16,15 @@ class TaskList(ListView):
 
 class TaskDetail(DetailView):
     model = Task
+    context_object_name = 'task'
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    # redirrect Value 
+    success_url = reverse_lazy('tasks')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
