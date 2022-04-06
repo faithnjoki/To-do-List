dd11bd86725e5ed9d3f0fbbcc249a7404914f5bb
Faith Muthoni <faith@Faiths-MacBook-Pro.local>
2022-04-06 16:45:51 +0300

    login view

--- to_do/app_todo/urls.py
+++ to_do/app_todo/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from  .views import TaskList,TaskDetail,TaskCreate,TaskUpdate,DetailView,DeleteView
+from  .views import TaskList,TaskDetail,TaskCreate,TaskUpdate,DetailView,DeleteView,CustomLogiView
 
 
 urlpatterns = [
+    path('login/', CustomLogiView.as_view(), name= 'login'),
     path('', TaskList.as_view(), name = 'tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name = 'task'),
     path('task-create/', TaskCreate.as_view(), name = 'task-create'),
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name ='task-update'),
-    path('task-delete/<int:pk>/', DeleteView.as_view(), name ='task-delete')
+    path('task-delete/<int:pk>/', DeleteView.as_view(), name ='task-delete'),
+    
 
 ]
