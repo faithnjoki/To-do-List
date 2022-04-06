e8440fa7d41f4a0114aa005e25e39578006887d7
Faith Muthoni <faith@Faiths-MacBook-Pro.local>
2022-04-06 21:14:32 +0300

    set the login_url to login in settingd

--- to_do/to_do/settings.py
+++ to_do/to_do/settings.py
@@ -113,6 +113,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+# if user is not authenticated you redirrect them to login 
+LOGIN_URL = 'login'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
